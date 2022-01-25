8b084a3eb290aabb1b6cec547ecb1446eb4528b6
91891 <91891@LAPTOP-UHV80U0P>
2022-01-25 12:45:20 +0530

    sample code

---
+++ src/com/jhhr/git/GitHub.java
@@ -0,0 +1,22 @@
+package com.jhhr.git;
+
+import java.util.ArrayList;
+
+public class GitHub 
+{
+	
+
+		public static void main(String[] args)
+		{
+		
+			ArrayList<String> AL = new ArrayList();
+			AL.add("hari");
+			AL.add("hara");
+			for(String list:AL)
+			{
+				System.out.println(list);
+			}
+
+		}
+
+}
